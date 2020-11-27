2cf0404e0010a6aac69070f4f4e8f9a5c29f33dc
Christopher Bartling <chris.bartling@gmail.com>
2020-11-26 23:37:38 -0600

    Wired in Firebase Auth and FirebaseUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import './App.css';
 import RoutesSwitch from "./RoutesSwitch";
-import {BrowserRouter as Router, Link} from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 function App() {
   return (
     <Router>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/welcome">Welcome</Link>
-      </div>
       <RoutesSwitch />
     </Router>
   );
--- src/views/FrontDoorView.tsx
+++ src/views/FrontDoorView.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import {StyledFirebaseAuth} from 'react-firebaseui';
+import firebase from 'firebase';
+import 'firebase/auth';
+
+// Configure FirebaseUI.
+const uiConfig = {
+
+  // Popup signin flow rather than redirect flow.
+  signInFlow: 'popup',
+
+  // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+  signInSuccessUrl: '/welcome',
+
+  // We will display Google as an auth providers.
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+  ]
+};
 
 function FrontDoorView() {
   return (
     <div>
       <h1>Front Door</h1>
+      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()}/>
     </div>
   );
 }
