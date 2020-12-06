224c7560da69b185f485b60374f87ea31b13fa67
Christopher Bartling <chris.bartling@gmail.com>
2020-12-06 13:10:33 -0600

    Upgrading dependencies: @apollo/client@3.3.4

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import {store} from '../MobxStateTree';
-import {getSnapshot} from 'mobx-state-tree';
 import ProfilePicture from "./ProfilePicture";
 
 function Header() {
-  const snapshot = getSnapshot(store);
-
   return (
     <div
       className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
@@ -14,7 +10,6 @@ function Header() {
       <nav className="my-2 my-md-0 mr-md-3">
         <Link className="p-2 text-dark" to="/welcome">Home</Link>
       </nav>
-      {/*<a className="btn btn-outline-primary" href="#">Sign up</a>*/}
       <ProfilePicture/>
     </div>
   );
--- src/components/ProfilePicture.tsx
+++ src/components/ProfilePicture.tsx
@@ -6,9 +6,13 @@ import './ProfilePicture.scss';
 function ProfilePicture() {
   const snapshot = getSnapshot(store);
 
-  return (
-    <img className="profile-picture" src={snapshot.currentUserModel?.photoURL} alt="Profile"/>
-  );
+  if (snapshot.currentUserModel?.photoURL) {
+    return (
+      <img className="profile-picture" src={snapshot.currentUserModel?.photoURL} alt="Profile"/>
+    );
+  } else {
+    return null;
+  }
 }
 
 export default ProfilePicture;
