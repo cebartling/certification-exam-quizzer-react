e25955b6379155c7d92b52578ca400a011a09218
Christopher Bartling <chris.bartling@gmail.com>
2020-12-31 23:48:00 -0600

    Building out form modal

--- src/views/examQuestionEditor/ExamQuestionResponseFormModal.tsx
+++ src/views/examQuestionEditor/ExamQuestionResponseFormModal.tsx
@@ -1,7 +1,6 @@
 import React, {useState} from 'react';
 import {Button, Form, Modal} from 'react-bootstrap';
 import {useForm} from 'react-hook-form';
-import {toast} from 'react-toastify';
 
 type ExamQuestionResponseFormModalProps = {
   examQuestion: any,
@@ -9,85 +8,82 @@ type ExamQuestionResponseFormModalProps = {
 
 function ExamQuestionResponseFormModal({examQuestion}: ExamQuestionResponseFormModalProps) {
   const [show, setShow] = useState(false);
-  const {register, handleSubmit, errors} = useForm();
+  const {handleSubmit, register} = useForm();
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  const closeDialog = () => setShow(false);
+  const showDialog = () => setShow(true);
 
   const onSubmit = async (formData: any) => {
-    try {
-    //   const result = await createExamQuestion({
-    //     variables: {
-    //       input: {
-    //         responseText: watch('responseText'),
-    //         explanationText: watch('explanationText'),
-    //         correct: watch('isCorrect') === 'correct',
-    //         examQuestionId: examQuestion.id,
-    //         clientMutationId: uuidv4()
-    //       }
-    //     }
-    //   });
-    //   examQuestionId = result.data.createExamQuestion.examQuestion.id;
-    //   // history.push(`/certification-exam/${certificationExamId}/questions?shouldRefetch=true`);
-      toast.success(`Created new exam question response.`, {position: toast.POSITION.BOTTOM_RIGHT});
-      setShow(false);
-    } catch (e) {
-      const message = 'Unable to create new exam question response.';
-      console.error(message, e);
-      toast.error(message, {position: toast.POSITION.BOTTOM_RIGHT});
-    }
+    console.info('Submitting the form');
+    // try {
+    //   //   const result = await createExamQuestion({
+    //   //     variables: {
+    //   //       input: {
+    //   //         responseText: watch('responseText'),
+    //   //         explanationText: watch('explanationText'),
+    //   //         correct: watch('isCorrect') === 'correct',
+    //   //         examQuestionId: examQuestion.id,
+    //   //         clientMutationId: uuidv4()
+    //   //       }
+    //   //     }
+    //   //   });
+    //   //   examQuestionId = result.data.createExamQuestion.examQuestion.id;
+    //   //   // history.push(`/certification-exam/${certificationExamId}/questions?shouldRefetch=true`);
+    //   toast.success(`Created new exam question response.`, {position: toast.POSITION.BOTTOM_RIGHT});
+    //   setShow(false);
+    // } catch (e) {
+    //   const message = 'Unable to create new exam question response.';
+    //   console.error(message, e);
+    //   toast.error(message, {position: toast.POSITION.BOTTOM_RIGHT});
+    // }
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Button variant="primary" onClick={handleShow}>
+    <div
+      onKeyDown={e => e.stopPropagation()}
+      onClick={e => e.stopPropagation()}
+      onFocus={e => e.stopPropagation()}
+      onMouseOver={e => e.stopPropagation()}
+      onSubmit={e => e.stopPropagation()}>
+      <Button variant="primary" onClick={showDialog}>
         Add response
       </Button>
 
-      <Modal show={show} onHide={handleClose} size="xl">
+      <Modal show={show} onHide={closeDialog} size="xl">
         <Modal.Header closeButton>
           <Modal.Title>Exam question response</Modal.Title>
         </Modal.Header>
-        <Modal.Body>
-          <div className="form-group row">
-            <label htmlFor="responseText"
-                   className="col-sm-2 col-form-label">Response text</label>
-            <div className="col-sm-10">
-              <textarea
-                rows={8}
-                name="responseText"
-                id="responseText"
-                ref={register({required: true})}
-                className="form-control">
-              </textarea>
-              {errors.responseText && <span className="text-danger">This field is required</span>}
-            </div>
-          </div>
-          <div className="form-group row">
-            <label htmlFor="explanationText"
-                   className="col-sm-2 col-form-label">Response text</label>
-            <div className="col-sm-10">
-              <textarea
-                rows={8}
-                name="explanationText"
-                id="explanationText"
-                ref={register({required: false})}
-                className="form-control">
-              </textarea>
-              {/*{errors.explanationText && <span className="text-danger">This field is required</span>}*/}
-            </div>
-          </div>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Cancel
-          </Button>
-          <Button variant="primary" type="submit">
-            Save
-          </Button>
-        </Modal.Footer>
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <Modal.Body>
+            <Form.Group controlId="responseText">
+              <Form.Label>Response text</Form.Label>
+              <Form.Control name="responseText" as="textarea" ref={register({required: true})}/>
+              <Form.Text className="text-muted">
+                This field is required.
+              </Form.Text>
+            </Form.Group>
+            <Form.Group controlId="explanationText">
+              <Form.Label>Explanation text</Form.Label>
+              <Form.Control name="explanationText" as="textarea" ref={register({required: false})}/>
+              <Form.Text className="text-muted">
+                This field is not required.
+              </Form.Text>
+            </Form.Group>
+            <Form.Group id="correctResponse">
+              <Form.Check type="checkbox" label="Correct response"/>
+            </Form.Group>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={closeDialog}>
+              Cancel
+            </Button>
+            <Button variant="primary" type="submit">
+              Save
+            </Button>
+          </Modal.Footer>
+        </Form>
       </Modal>
-    </form>
+    </div>
   );
 }
 
