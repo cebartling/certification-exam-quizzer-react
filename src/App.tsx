7f9da0b8a46a1c4b816f4e8aa395ee391491e198
Christopher Bartling <chris.bartling@gmail.com>
2021-01-18 08:49:30 -0600

    Small refactoring to use useSetRecoilState hook.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {useSetRecoilState} from 'recoil';
 import {ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './App.scss';
@@ -7,19 +8,17 @@ import {BrowserRouter as Router} from 'react-router-dom';
 import Header from './components/Header';
 import Footer from "./components/Footer";
 import fontawesome from '@fortawesome/fontawesome';
-import {faEdit, faTrash, faPlus} from '@fortawesome/fontawesome-free-solid';
+import {faEdit, faPlus, faTrash} from '@fortawesome/fontawesome-free-solid';
 import initializeFirebaseApp from './FirebaseInitialization';
-import {useRecoilState} from 'recoil';
 import {userState} from './recoil/atoms';
 
 fontawesome.library.add(faEdit, faTrash, faPlus);
 
 function App() {
-  // @ts-ignore
-  const userRecoilStateArray = useRecoilState(userState);
+  const setUserState = useSetRecoilState(userState);
 
   window.addEventListener('load', function () {
-    initializeFirebaseApp(userRecoilStateArray[1]);
+    initializeFirebaseApp(setUserState);
   });
 
   return (
